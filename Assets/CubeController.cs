54366b106ac3e30411f44f3213b8c58025f1e2eb
KeiP <keittisakcpm@gmail.com>
2020-02-13 15:30:02 +0700

    first commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 target = Vector3.forward;
+    float speed = 5f;
+    void Start()
+    {
+        transform.RotateAround(this.transform.position, target, speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.RotateAround(this.transform.position, target, speed * Time.deltaTime);
+    }
+}
