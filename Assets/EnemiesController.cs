14ace107211ca15798f1966aeca27c42cc34b246
KeiP <keittisakcpm@gmail.com>
2020-02-13 21:17:44 +0700

    Update

---
+++ Assets/EnemiesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesController : MonoBehaviour
+{
+    public GameObject earth;
+    public float speed;
+    void Start()
+    {
+        earth = GameObject.Find("Earth");
+    }
+
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, earth.transform.position, speed * Time.deltaTime);
+
+        Vector3 AmaPosition = earth.transform.position;
+        AmaPosition = Camera.main.ScreenToWorldPoint(AmaPosition);
+
+        Vector2 direction = new Vector2(
+            AmaPosition.x = transform.position.x,
+            AmaPosition.y = transform.position.y
+        );
+        transform.left = direction;
+    }
+}
---
+++ Assets/EnimiesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnimiesController : MonoBehaviour
+{
+
+    public GameObject earth;
+    public float speed;
+
+    private void Start()
+    {
+        earth = GameObject.Find("Earth");
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,earth.transform.position,speed*Time.deltaTime);
+
+        Vector3 MeoPosition = earth.transform.position;
+       
+
+        Vector2 direction = new Vector2(
+            MeoPosition.x = transform.position.x,
+            MeoPosition.y = transform.position.y            
+            );
+
+        transform.up = direction;
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+}
